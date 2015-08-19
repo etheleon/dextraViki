f2ffa95bd84e50d13ea39cd93ec7a2fe63d098fd
ritikakapoor22 <ritika.kapoor22@gmail.com>
2015-08-19 20:35:25 +0800

    Merged User info with score

--- script/viki.0101.someAnalysis.R
+++ script/viki.0101.someAnalysis.R
@@ -9,4 +9,12 @@ userdetails_all<-merge(behave, users, by="user_id", all.x=TRUE)
 #Merge Userall details with video details (except the casts)
 user_video_nocast<- merge(userdetails_all, videos_attributes, by="video_id", all.x=TRUE)
 
-# Things we want to do now: 1) See how the user and video clusters
+#trying things 16th August
+#group the users by Score country and gender
+user_score <- user_video_nocast %>% group_by(user_id) %>% summarise(totalScore = sum(score)) %>% arrange(desc(totalScore))
+uu = merge(user_score, users)
+
+#make a plot and save it in the pdf
+pdf("~/Desktop/somePlot.pdf", width=20)
+ggplot(uu, aes(gender, totalScore)) + geom_boxplot() + geom_jitter(aes(color=country))
+dev.off()
